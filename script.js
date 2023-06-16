147b616fcc270d86d8399c487c459b42787ff26d
Michael Flanagan <mflanagan924@gmail.com>
2023-06-16 17:42:54 +0200

    header, navbar, css, hamburger

--- script.js
+++ script.js
@@ -26,3 +26,13 @@ function mouseLeave() {
   }
 }
  */
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
